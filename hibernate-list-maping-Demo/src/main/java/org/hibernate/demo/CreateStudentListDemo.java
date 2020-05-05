ad1ea42b94d875b5e74fd4fea1b7da7dc0a27302
rps123-coder <rajkunwar3007@gmail.com>
2020-05-05 11:26:03 +0530

    Advance hibernate feature demo. List mapping

---
+++ hibernate-list-maping-Demo/src/main/java/org/hibernate/demo/CreateStudentListDemo.java
@@ -0,0 +1,39 @@
+package org.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.demo.entity.Student;
+
+public class CreateStudentListDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory=new Configuration().configure("hibernate.cfg.xml")
+				                                         .addAnnotatedClass(Student.class).buildSessionFactory();
+		
+		Session session=sessionFactory.getCurrentSession();
+		
+		try{
+			Student theStudent=new Student("Luv", "Singh", "luvsingh@gmail.com");
+			List<String> image=theStudent.getImages();
+			
+			image.add("photo1.png");
+			image.add("photo2.png");
+			image.add("photo3.png");
+			image.add("photo4.png");
+			
+			session.beginTransaction();
+			
+			session.save(theStudent);
+			
+			session.getTransaction().commit();
+			
+		}finally {
+			sessionFactory.close();
+		}
+	}
+
+}
